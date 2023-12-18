868c89f765553a7f9f96d7ea930234ad8cb35e7e
Milesm <miles1301@icloud.com>
2023-12-18 18:01:47 -0500

    app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let currentTargetIndex = 1;
     
     const targetDates = [
         new Date('November 16, 2023 8:00:15'),
-        new Date('December 19, 2023 00:00:00'),
+        new Date('December 20, 2023 12:00:00'),
         new Date('March 17, 2024 00:00:00')
     ];
     
